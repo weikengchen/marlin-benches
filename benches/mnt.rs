b5e7cd6b8c3c9b9d94a01020aee41623daa081c2
weikeng <w.k@berkeley.edu>
2020-06-02 19:59:38 -0700

    new bencher

--- benches/mnt.rs
+++ benches/mnt.rs
@@ -4,25 +4,48 @@ use rand;
 extern crate criterion;
 
 use algebra::fields::PrimeField;
-use algebra::One;
-use algebra::{mnt4_298::{MNT4_298, Fr as MNT4Fr}, mnt6_298::{MNT6_298, Fr as MNT6Fr}};
-use criterion::Criterion;
-use marlin::{Marlin, IndexProverKey, UniversalSRS};
-use r1cs_core::{ConstraintSynthesizer, ConstraintSystem, SynthesisError};
-use blake2::Blake2s;
-use poly_commit::marlin_kzg10::MarlinKZG10;
+use algebra::{
+    bls12_381::{Bls12_381, Fr as BlsFr},
+    mnt4_298::{Fr as MNT4Fr, MNT4_298},
+    mnt4_753::{Fr as MNT4BigFr, MNT4_753},
+    mnt6_298::{Fr as MNT6Fr, MNT6_298},
+    mnt6_753::{Fr as MNT6BigFr, MNT6_753},
+};
 use algebra_core::UniformRand;
+use blake2::Blake2s;
+use criterion::Criterion;
+use marlin::Marlin;
+use poly_commit::marlin_kzg10::MarlinKZG10;
+use r1cs_core::{ConstraintSynthesizer, ConstraintSystem, SynthesisError};
 
 type MultiPcMnt4 = MarlinKZG10<MNT4_298>;
 type MultiPcMnt6 = MarlinKZG10<MNT6_298>;
+type MultiPcBls = MarlinKZG10<Bls12_381>;
+type MultiPcMnt4Big = MarlinKZG10<MNT4_753>;
+type MultiPcMnt6Big = MarlinKZG10<MNT6_753>;
+
 type MarlinInstMnt4 = Marlin<MNT4Fr, MultiPcMnt4, Blake2s>;
 type MarlinInstMnt6 = Marlin<MNT6Fr, MultiPcMnt6, Blake2s>;
+type MarlinInstBls = Marlin<BlsFr, MultiPcBls, Blake2s>;
+type MarlinInstMnt4Big = Marlin<MNT4BigFr, MultiPcMnt4Big, Blake2s>;
+type MarlinInstMnt6Big = Marlin<MNT6BigFr, MultiPcMnt6Big, Blake2s>;
 
 struct DummyCircuit<F: PrimeField> {
     pub a: Option<F>,
     pub b: Option<F>,
     pub num_variables: usize,
     pub num_constraints: usize,
+}
+
+impl<F: PrimeField> Clone for DummyCircuit<F> {
+    fn clone(&self) -> Self {
+        DummyCircuit {
+            a: self.a.clone(),
+            b: self.b.clone(),
+            num_variables: self.num_variables,
+            num_constraints: self.num_constraints,
+        }
+    }
 }
 
 impl<F: PrimeField> ConstraintSynthesizer<F> for DummyCircuit<F> {
@@ -50,7 +73,7 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for DummyCircuit<F> {
             )?;
         }
 
-        for i in 0..self.num_constraints - 1{
+        for i in 0..self.num_constraints - 1 {
             cs.enforce(
                 || format!("constraint {}", i),
                 |lc| lc + a,
@@ -59,119 +82,98 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for DummyCircuit<F> {
             );
         }
 
-        cs.enforce(
-            || "constraint {}",
-            |lc| lc,
-            |lc| lc,
-            |lc| lc,
-        );
+        cs.enforce(|| "constraint {}", |lc| lc, |lc| lc, |lc| lc);
 
         Ok(())
     }
 }
-
-fn index_mnt4<'a>(num_constraints: usize) -> UniversalSRS<MNT4Fr, MultiPcMnt4> {
-    let rng = &mut rand::thread_rng();
-    let srs = MarlinInstMnt4::universal_setup(num_constraints, 10, num_constraints , rng).unwrap();
-    srs
-}
-
-fn index_mnt6<'a>(num_constraints: usize) -> UniversalSRS<MNT6Fr, MultiPcMnt6> {
-    let rng = &mut rand::thread_rng();
-    let srs = MarlinInstMnt6::universal_setup(num_constraints , 10, num_constraints, rng).unwrap();
-    srs
-}
-
-fn prepare_mnt4<'a>(num_constraints: usize, srs: &'a UniversalSRS<MNT4Fr, MultiPcMnt4>) -> IndexProverKey<'a, MNT4Fr, MultiPcMnt4, DummyCircuit<MNT4Fr>> {
-    let rng = &mut rand::thread_rng();
-    let c = DummyCircuit::<MNT4Fr> { a:Some(MNT4Fr::rand(rng)), b:Some(MNT4Fr::rand(rng)), num_variables: 10, num_constraints: num_constraints };
-
-    let (pk, _) = MarlinInstMnt4::index(&srs, c).unwrap();
-
-    pk
-}
-
-fn prepare_mnt6<'a>(num_constraints: usize, srs: &'a UniversalSRS<MNT6Fr, MultiPcMnt6>) -> IndexProverKey<'a, MNT6Fr, MultiPcMnt6, DummyCircuit<MNT6Fr>> {
-    let rng = &mut rand::thread_rng();
-    let c = DummyCircuit::<MNT6Fr> { a:Some(MNT6Fr::rand(rng)), b:Some(MNT6Fr::rand(rng)), num_variables: 10, num_constraints: num_constraints };
-
-    let (pk, _) = MarlinInstMnt6::index(&srs, c).unwrap();
-
-    pk
-}
-
-fn bench_prove_mnt4<'a>(num_constraints: usize, ipk: &IndexProverKey<'a, MNT4Fr, MultiPcMnt4, DummyCircuit<MNT4Fr>>) {
-    let rng = &mut rand::thread_rng();
-    let c = DummyCircuit::<MNT4Fr> { a: Some(MNT4Fr::rand(rng)), b:Some(MNT4Fr::rand(rng)), num_variables: 10, num_constraints: num_constraints };
-
-    let _ = MarlinInstMnt4::prove(ipk, c, rng).unwrap();
-}
-
-fn bench_prove_mnt6<'a>(num_constraints: usize, ipk: &IndexProverKey<'a, MNT6Fr, MultiPcMnt6, DummyCircuit<MNT6Fr>>) {
-    let rng = &mut rand::thread_rng();
-    let c = DummyCircuit::<MNT6Fr> { a: Some(MNT6Fr::rand(rng)), b:Some(MNT6Fr::rand(rng)),  num_variables: 10, num_constraints: num_constraints };
-
-    let _ = MarlinInstMnt6::prove(ipk, c, rng).unwrap();
-}
-
-fn bench_prove_mnt4_2_10_to_2_20(c: &mut Criterion) {
-    for i in [
-        1024usize,
-        2048usize,
-        4096usize,
-        8192usize,
-        16384usize,
-        32768usize,
-        65536usize,
-        131072usize,
-        262144usize,
-        524288usize,
-        1048576usize,
-    ]
-    .iter()
+fn bench_prove(cr: &mut Criterion) {
     {
-        let srs = index_mnt4(i.clone());
-        let params = prepare_mnt4(i.clone(), &srs);
-        c.bench_function(&format!("{}", i), |b| {
-            b.iter(|| bench_prove_mnt4(i.clone(), &params.clone()))
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<BlsFr> {
+            a: Some(BlsFr::rand(rng)),
+            b: Some(BlsFr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstBls::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, _) = MarlinInstBls::index(&srs, c.clone()).unwrap();
+
+        cr.bench_function(&"bls", |b| {
+            b.iter(|| MarlinInstBls::prove(&pk, c.clone(), rng).unwrap())
         });
     }
-}
-
-fn bench_prove_mnt6_2_10_to_2_20(c: &mut Criterion) {
-    for i in [
-        1024usize,
-        2048usize,
-        4096usize,
-        8192usize,
-        16384usize,
-        32768usize,
-        65536usize,
-        131072usize,
-        262144usize,
-        524288usize,
-        1048576usize,
-    ]
-    .iter()
     {
-        let srs = index_mnt6(i.clone());
-        let params = prepare_mnt6(i.clone(), &srs);
-        c.bench_function(&format!("{}", i), |b| {
-            b.iter(|| bench_prove_mnt6(i.clone(), &params.clone()))
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<MNT4Fr> {
+            a: Some(MNT4Fr::rand(rng)),
+            b: Some(MNT4Fr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstMnt4::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, _) = MarlinInstMnt4::index(&srs, c.clone()).unwrap();
+
+        cr.bench_function(&"mnt4", |b| {
+            b.iter(|| MarlinInstMnt4::prove(&pk, c.clone(), rng).unwrap())
+        });
+    }
+    {
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<MNT6Fr> {
+            a: Some(MNT6Fr::rand(rng)),
+            b: Some(MNT6Fr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstMnt6::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, _) = MarlinInstMnt6::index(&srs, c.clone()).unwrap();
+
+        cr.bench_function(&"mnt6", |b| {
+            b.iter(|| MarlinInstMnt6::prove(&pk, c.clone(), rng).unwrap())
+        });
+    }
+    {
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<MNT4BigFr> {
+            a: Some(MNT4BigFr::rand(rng)),
+            b: Some(MNT4BigFr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstMnt4Big::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, _) = MarlinInstMnt4Big::index(&srs, c.clone()).unwrap();
+
+        cr.bench_function(&"mnt4Big", |b| {
+            b.iter(|| MarlinInstMnt4Big::prove(&pk, c.clone(), rng).unwrap())
+        });
+    }
+    {
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<MNT6BigFr> {
+            a: Some(MNT6BigFr::rand(rng)),
+            b: Some(MNT6BigFr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstMnt6Big::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, _) = MarlinInstMnt6Big::index(&srs, c.clone()).unwrap();
+
+        cr.bench_function(&"mnt6Big", |b| {
+            b.iter(|| MarlinInstMnt6Big::prove(&pk, c.clone(), rng).unwrap())
         });
     }
 }
 
 criterion_group! {
-    name = mnt4;
+    name = marlin;
     config = Criterion::default().sample_size(10);
-    targets = bench_prove_mnt4_2_10_to_2_20
+    targets = bench_prove
 }
 
-criterion_group! {
-    name = mnt6;
-    config = Criterion::default().sample_size(10);
-    targets = bench_prove_mnt6_2_10_to_2_20
-}
-
-criterion_main!(mnt4, mnt6);
+criterion_main!(marlin);
