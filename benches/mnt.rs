531636b8e31785d827b82582039d1622799f730b
weikeng <w.k@berkeley.edu>
2020-06-03 09:21:30 -0700

    add verify bench

--- benches/mnt.rs
+++ benches/mnt.rs
@@ -87,6 +87,7 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for DummyCircuit<F> {
         Ok(())
     }
 }
+
 fn bench_prove(cr: &mut Criterion) {
     {
         let rng = &mut rand::thread_rng();
@@ -170,10 +171,108 @@ fn bench_prove(cr: &mut Criterion) {
     }
 }
 
+fn bench_verify(cr: &mut Criterion) {
+    {
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<BlsFr> {
+            a: Some(BlsFr::rand(rng)),
+            b: Some(BlsFr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstBls::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, vk) = MarlinInstBls::index(&srs, c.clone()).unwrap();
+        let proof = MarlinInstBls::prove(&pk, c.clone(), rng).unwrap();
+
+        cr.bench_function(&"bls", |b| {
+            b.iter(|| MarlinInstBls::verify(&vk, &vec![BlsFr::rand(rng)], &proof, rng).unwrap())
+        });
+    }
+    {
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<MNT4Fr> {
+            a: Some(MNT4Fr::rand(rng)),
+            b: Some(MNT4Fr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstMnt4::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, vk) = MarlinInstMnt4::index(&srs, c.clone()).unwrap();
+        let proof = MarlinInstMnt4::prove(&pk, c.clone(), rng).unwrap();
+
+        cr.bench_function(&"mnt4", |b| {
+            b.iter(|| MarlinInstMnt4::verify(&vk, &vec![MNT4Fr::rand(rng)], &proof, rng).unwrap())
+        });
+    }
+    {
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<MNT6Fr> {
+            a: Some(MNT6Fr::rand(rng)),
+            b: Some(MNT6Fr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstMnt6::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, vk) = MarlinInstMnt6::index(&srs, c.clone()).unwrap();
+        let proof = MarlinInstMnt6::prove(&pk, c.clone(), rng).unwrap();
+
+        cr.bench_function(&"mnt6", |b| {
+            b.iter(|| MarlinInstMnt6::verify(&vk, &vec![MNT6Fr::rand(rng)], &proof, rng).unwrap())
+        });
+    }
+    {
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<MNT4BigFr> {
+            a: Some(MNT4BigFr::rand(rng)),
+            b: Some(MNT4BigFr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstMnt4Big::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, vk) = MarlinInstMnt4Big::index(&srs, c.clone()).unwrap();
+        let proof = MarlinInstMnt4Big::prove(&pk, c.clone(), rng).unwrap();
+
+        cr.bench_function(&"mnt4Big", |b| {
+            b.iter(|| {
+                MarlinInstMnt4Big::verify(&vk, &vec![MNT4BigFr::rand(rng)], &proof, rng).unwrap()
+            })
+        });
+    }
+    {
+        let rng = &mut rand::thread_rng();
+        let c = DummyCircuit::<MNT6BigFr> {
+            a: Some(MNT6BigFr::rand(rng)),
+            b: Some(MNT6BigFr::rand(rng)),
+            num_variables: 10,
+            num_constraints: 65536,
+        };
+
+        let srs = MarlinInstMnt6Big::universal_setup(65536, 10, 65536, rng).unwrap();
+        let (pk, vk) = MarlinInstMnt6Big::index(&srs, c.clone()).unwrap();
+        let proof = MarlinInstMnt6Big::prove(&pk, c.clone(), rng).unwrap();
+
+        cr.bench_function(&"mnt6Big", |b| {
+            b.iter(|| {
+                MarlinInstMnt6Big::verify(&vk, &vec![MNT6BigFr::rand(rng)], &proof, rng).unwrap()
+            })
+        });
+    }
+}
+
 criterion_group! {
-    name = marlin;
+    name = marlin_prove;
     config = Criterion::default().sample_size(10);
     targets = bench_prove
 }
 
-criterion_main!(marlin);
+criterion_group! {
+    name = marlin_verify;
+    config = Criterion::default().sample_size(30);
+    targets = bench_verify
+}
+
+criterion_main!(marlin_verify);
